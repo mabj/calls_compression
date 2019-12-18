cee401167d733dabd09170cbbba19f6486f7e6c2
Marcos Alvares <marcos.alvares@fireeye.com>
2019-12-18 01:42:38 +0100

    adding examples

---
+++ examples/dummy_2gram.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+from common import *
+
+def main(argc=0, argv=[]):
+    generic_call(1, lineno())
+    for i in range(3):
+        generic_call(2, lineno())
+        generic_call(3, lineno())
+    generic_call(4, lineno())
+
+if __name__ == '__main__':
+    sys.exit(main(len(sys.argv), sys.argv))
